4c66a7bf20107599ae9a8f41776381ecdf68eb6b
Rusif Eyvazli <eyvazlirusif@gmail.com>
2022-06-02 11:26:37 -0400

    Removed Addlistener

--- index.js
+++ index.js
@@ -69,12 +69,3 @@ $(document).ready(function() {
     var viewModel = new customerDetailsViewModel();
     ko.applyBindings(viewModel);
  });
-
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
